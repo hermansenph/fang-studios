89c431b776a2489f29c2b255254f17c07a22c33c
hermansenph <hermansenph@gmail.com>
2018-04-08 04:58:10 -0700

    Add environment variable for port

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const app = express()
 
 app
   .use(express.static(path.join(__dirname, 'public')))
-  .listen(3000, () => {
+  .listen(process.env.port || 3000, () => {
     console.log('Listening on :3000')
   })
