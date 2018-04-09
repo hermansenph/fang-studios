c20b42b30102c90746ae5a7b07460b07e4272a81
hermansenph <hermansenph@gmail.com>
2018-04-08 18:44:19 -0700

    Updated app.js #2

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderApp from './apps/header-app'
 import ZvhApp from './apps/zvh-app'
+import SupportApp from './apps/support-app'
 
 export default class App extends React.Component {
 
@@ -10,12 +11,25 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <HeaderApp />
-        <ZvhApp />
-      </div>
-    )
+
+    if (!window.location.hash) {
+      return (
+        <div>
+          <HeaderApp />
+          <ZvhApp />
+        </div>
+      )
+    }
+
+    if (window.location.hash === '#support') {
+      return (
+        <div>
+          <HeaderApp />
+          <SupportApp />
+        </div>
+      )
+    }
+
   }
 
 }
